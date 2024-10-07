663592cd093c5c08ed0f8a947276f190728e75b7
Arifin-only <arifin180104@gmail.com>
2024-10-07 21:02:36 +0800

    Create Tanpa_Ternary_Operator.dart

---
+++ Tanpa_Ternary_Operator.dart
@@ -0,0 +1,12 @@
+ void main(){
+   var nilai = 70;
+   String katakata;
+
+   if(nilai >= 60){
+     katakata = 'Anjay nilai anda masih bagus';
+   }else{
+     katakata = 'Waduh harus di tingkatkan lagi nih';
+   }
+
+   print(katakata);
+ }
